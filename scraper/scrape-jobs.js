bd5c4c02d11fdf46190faea194a214443af686df
Ed <edwoulfe@gmail.com>
2023-07-06 11:55:11 -0400

    Successfully am scrolling the jobs listings on LinkedIn and navigating to the next page. Each page of listings is made up of 25 or 24 jobs. So when the scraper gets to the end of the list, we just go to the next page.

--- scraper/scrape-jobs.js
+++ scraper/scrape-jobs.js
@@ -7,51 +7,151 @@ const jobTitle = "Front End Developer";
 const jobLocation = "United States";
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: ['--window-size=1620,900'], });
     const page = await browser.newPage();
-    // await linkedInLogin(page);
-    // await linkedInJobs24Hours(page);
+
+
+    await linkedInLogin(page);
+    await new Promise(r => setTimeout(r, 15000));
+
+    await linkedInJobs24Hours(page, jobTitle, jobLocation, 0);
+    await new Promise(r => setTimeout(r, 5000));
+    await scrapeJobs(page, browser);
     // await glassDoorLogin(page);
     // await queryGlassDoorJobs(page);
     // await indeedJobs24Hours(page);
-    await googleJobs24Hours(page);
+    // await googleJobs24Hours(page);
 
 
-    // await new Promise(r => setTimeout(r, 10000));
+    // await new Promise(r => setTimeout(r, 3000));
     // await browser.close();
 })();
 
+async function linkedInJobs24Hours(page, title, location, start) {
+    let queryTitle = title.replaceAll(' ', '%20');
+    let queryLocation = location.replaceAll(' ', '%20').replaceAll(',', '%2C');
+
+    if (start === 0) {
+        await page.goto(`https://www.linkedin.com/jobs/search/?keywords=${queryTitle}&location=${queryLocation}&f_TPR=r86400&refresh=true`, {
+            waitUntil: "domcontentloaded",
+        });
+    } else {
+        await page.goto(`https://www.linkedin.com/jobs/search/?keywords=${queryTitle}&location=${queryLocation}&f_TPR=r86400&refresh=true&start=${start}`, {
+            waitUntil: "domcontentloaded",
+        });
+    }
+
+
+
+}
+async function scrapeJobs(page, browser) {
+    //listContainer, cardContainer, jobTitle, jobLocation, companyName, applyNowButton, jobDescription
+
+
+
+    const listContainer = '#main > div > div.scaffold-layout__list > div > ul';
+
+    const cardContainer = '#main > div > div.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details';
+
+    const applyButtonSelector = '#main > div > div.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details div.relative.jobs-unified-top-card__container--two-pane > div.jobs-unified-top-card__content--two-pane > div:nth-child(4) > div > div > div button';
+
+
+
+    const listLimit = await page.evaluate(() => {
+        let numOfResults = document.querySelector("#main > div > div.scaffold-layout__list > header > div.jobs-search-results-list__title-heading > small > div > span").innerText;
+        return Number(numOfResults.split(' ')[0].replace(',', ''));
+
+    })
+
+
+    let numOfListItems = await page.evaluate(() => {
+        return document.querySelectorAll('#main > div > div.scaffold-layout__list > div > ul > li').length
+    });
+    let pageNumber = 1;
+
+
+
+
+    for (let i = 1; i <= listLimit; i++) {
+
+        let listElement = `${listContainer} > li:nth-child(${i})`;
+        console.log(listElement)
+
+
+
+        await page.click(`${listContainer} > li:nth-child(${i})`);
+        console.log("list item " + i)
+        await new Promise(r => setTimeout(r, 5000));
+
+
+        // let element = await page.waitForSelector(applyButtonSelector);
+        // let applyButtonText = await element.evaluate(el => el.textContent);
+
+        // // Job Title
+        // let cardJobTitle = await page.evaluate((cardContainer) => {
+        //     return document.querySelector(`${cardContainer} h2`).innerText;
+        // }, cardContainer)
+        // console.log(cardJobTitle)
+        // if (applyButtonText.trim() === "Easy Apply") {
+        //     console.log("easy found")
+        //     await new Promise(r => setTimeout(r, 2000));
+        //     continue;
+        // } else {
+        //     console.log('else')
+
+        //     await page.click(applyButtonSelector);
+        //     await new Promise(r => setTimeout(r, 7000));
+        //     const pages = await browser.pages()
+        //     const applyLink = pages[2].url();
+        //     console.log(applyLink);
+        //     await pages[2].close();
+        // }
+        if (i === numOfListItems) {
+            linkedInJobs24Hours(page, jobTitle, jobLocation, pageNumber * 25)
+            await new Promise(r => setTimeout(r, 5000));
+            pageNumber += 1;
+
+            i = 0;
+            numOfListItems = await page.evaluate(() => {
+                return document.querySelectorAll('#main > div > div.scaffold-layout__list > div > ul > li').length
+            });
+            console.log(numOfListItems)
+            await new Promise(r => setTimeout(r, 5000));
+
+        }
+
+
+
+    }
+
+    // // Company Name
+    // const jobInformation = document.querySelector('div.jobs-unified-top-card__primary-description').innerText;
+    // let infoArray = jobInformation.split('·');
+
+
+
+
+
+
+
+}
 
 //                  LinkedIn                \\
 async function linkedInLogin(page) {
-    await page.goto(jobBoardWebsites.linkedIn.URL, {
-        waitUntil: "domcontentloaded",
-    });
+    await page.goto(jobBoardWebsites.linkedIn.URL);
     page.type('#session_key', process.env.ACCOUNT_USERNAME);
     await new Promise(r => setTimeout(r, 1000));
     page.type('#session_password', process.env.ACCOUNT_PASSWORD);
     await new Promise(r => setTimeout(r, 1000));
     page.click('div.flex.justify-between.sign-in-form__footer--full-width > button')
-    await new Promise(r => setTimeout(r, 2000));
-}
-
-
-
-async function linkedInJobs24Hours(page) {
-    await new Promise(r => setTimeout(r, 2000));
-    page.type('#jobs-search-box-keyword-id-ember22', jobTitle);
-    await new Promise(r => setTimeout(r, 2000));
-    page.type('#jobs-search-box-location-id-ember22', jobLocation)
-    await new Promise(r => setTimeout(r, 1000));
-    page.keyboard.press('Enter');
-    await new Promise(r => setTimeout(r, 5000));
-    page.click('#ember329 > button');
-    await new Promise(r => setTimeout(r, 2000));
-    page.click('#timePostedRange-r86400');
-    await new Promise(r => setTimeout(r, 2000));
-    page.click('#ember533')
 
 }
+
+
+
+
+
+
 
 //------------------------------------------\\
 
@@ -83,8 +183,10 @@ async function queryGlassDoorJobs(page) {
     page.type('#scLocation', jobLocation)
     await new Promise(r => setTimeout(r, 1000));
     page.keyboard.press('Enter');
-    await new Promise(r => setTimeout(r, 4000));
+    await new Promise(r => setTimeout(r, 7000));
     page.click('#Discover > div > div > div.d-flex.flex-column.col-lg-8.col-12.mt-lg-0 > div:nth-child(1) > div.mt-std.d-flex.justify-content-center > a')
+    await new Promise(r => setTimeout(r, 3000));
+    return;
 
 }
 
@@ -128,6 +230,8 @@ async function indeedJobs24Hours(page) {
     page.click('#filter-dateposted');
     await new Promise(r => setTimeout(r, 2000));
     page.click('#filter-dateposted-menu > li:nth-child(1) > a')
+    await new Promise(r => setTimeout(r, 3000));
+    return;
 
 
 
@@ -152,6 +256,9 @@ async function googleJobs24Hours(page) {
     page.click('#choice_box_root > div.vk0qtc > div.w0rs6e > span:nth-child(5)');
     await new Promise(r => setTimeout(r, 2000))
     page.click('#choice_box_root > div.KzzVYe > div:nth-child(4) > div.JMgW3.tl-single-select > div:nth-child(2)')
+    await new Promise(r => setTimeout(r, 3000));
+    return;
 
 
 }
+
